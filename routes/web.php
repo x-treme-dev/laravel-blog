51b3e7d279df2c4dd761ab75ce3586c0bddb4514
x-treme <yurchenko-inform@yandex.ru>
2024-02-24 12:52:26 +0300

    added changes from github repo

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\TodoController;
+ 
 
 /*
 |--------------------------------------------------------------------------
@@ -24,4 +26,7 @@ Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/posts/{slug}', [PostController::class, 'getPost']);
 
+ 
+Route::get('/todo', [TodoController::class, 'index']);
+ 
  
